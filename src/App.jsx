8306a160d48e761234ba666a19fc0a54406322ee
thaihoaho <thaihoa.hn120@gmail.com>
2024-12-05 13:11:54 +0700

    Merge

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,11 @@ import DefaultLayout from "./components/layout/DefaultLayout.jsx";
 import Start from "./pages/start/start.jsx";
 import Login from "./pages/auth/signin.jsx";
 import Signup from "./pages/auth/signup.jsx";
-import MovieList from "./pages/movielist/movielist.jsx";
 import AddFilmPage from "./pages/config/addFilm/addFilm.jsx";
 import UpdateFilmPage from "./pages/config/updateFilm/updateFilm.jsx";
 import DeleteFilmPage from "./pages/config/deleteFilm/deleteFilm.jsx";
 import FilmManagementLayout from "./pages/config/layout.jsx";
-
+import MovieList from "./pages/movielist/movielist.jsx";
 function App() {
 
   return (
@@ -18,7 +17,11 @@ function App() {
         <Route path='/signin' element={<Login></Login>} />
         <Route path='/signup' element={<Signup></Signup>} />
         <Route path='/home' element={<DefaultLayout><Home></Home></DefaultLayout>} />
-
+        <Route path='/all' element={<DefaultLayout><MovieList></MovieList></DefaultLayout>} />
+        <Route path='/config' element={<FilmManagementLayout></FilmManagementLayout>} />
+        <Route path='/config/addfilm' element={<AddFilmPage></AddFilmPage>} />
+        <Route path='/config/updatefilm' element={<UpdateFilmPage></UpdateFilmPage>} />
+        <Route path='/config/deletefilm' element={<DeleteFilmPage></DeleteFilmPage>} />
 
       </Routes>
   )
