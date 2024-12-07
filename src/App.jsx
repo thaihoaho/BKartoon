316598ede1d5e3b2184ea3f1c30da83a88e5172e
ShaKk0722 <120005862+ShaKk0722@users.noreply.github.com>
2024-12-07 16:12:02 +0700

    done feat/auth

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import DefaultLayout from "./components/layout/DefaultLayout.jsx";
 import Start from "./pages/start/start.jsx";
 import Login from "./pages/auth/signin.jsx";
 import Signup from "./pages/auth/signup.jsx";
+import Role from "./pages/auth/role.jsx";
+import LoginAdmin from "./pages/auth/signinAdmin.jsx";
 import AddFilmPage from "./pages/config/addFilm/addFilm.jsx";
 import UpdateFilmPage from "./pages/config/updateFilm/updateFilm.jsx";
 // import DeleteFilmPage from "./pages/config/deleteFilm/deleteFilm.jsx";
@@ -11,13 +13,16 @@ import FilmManagementLayout from "./pages/config/layout.jsx";
 import MovieList from "./pages/movielist/movielist.jsx";
 import AnimeDetails from "./pages/info/info.jsx";
 
+
 function App() {
 
   return (
       <Routes>
         <Route path='/' element={<Start></Start>} />
-        <Route path='/signin' element={<Login></Login>} />
+        <Route path='/signin/user' element={<Login></Login>} />
+        <Route path='/signin/admin' element={<LoginAdmin></LoginAdmin>} />
         <Route path='/signup' element={<Signup></Signup>} />
+        <Route path='/role' element={<Role></Role>} />
         <Route path='/home' element={<DefaultLayout><Home></Home></DefaultLayout>} />
         <Route path='/all' element={<DefaultLayout><MovieList></MovieList></DefaultLayout>} />
         <Route path='/config' element={<FilmManagementLayout></FilmManagementLayout>} />
